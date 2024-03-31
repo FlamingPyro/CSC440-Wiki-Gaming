6d0cdbd6f1b7ded674d45d8de34d3265e12701de
DimitriS49 <163360382+DimitriS49@users.noreply.github.com>
2024-03-31 13:48:59 -0400

    Add files via upload

--- Riki/wiki/web/shopping_cart_db/shopping_cart_db.py
+++ Riki/wiki/web/shopping_cart_db/shopping_cart_db.py
@@ -1,1 +1,20 @@
+import sqlite3
 
+conn = sqlite3.connect('shopping_info.db')
+cursor = conn.cursor()
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS shopping_info (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    zipcode TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone_number TEXT NOT NULL
+)
+'''
+cursor.execute(create_table_query)
+
+conn.commit()
+conn.close()
