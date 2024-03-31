e5a5b0a30ba0d21695df2c5e473155e7829472da
DimitriS49 <163360382+DimitriS49@users.noreply.github.com>
2024-03-31 13:20:20 -0400

    Update Riki.py (Added lines for Database)

--- Riki/Riki.py
+++ Riki/Riki.py
@@ -2,9 +2,13 @@
 import os
 
 from wiki import create_app
+from wiki.web.extensions import db
 
 directory = os.getcwd()
 app = create_app(directory)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
