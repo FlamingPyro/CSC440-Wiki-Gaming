22b778d670523658bbcbb743bd2f0eeecfe145d3
DimitriS49 <163360382+DimitriS49@users.noreply.github.com>
2024-04-16 10:46:06 -0400

    Update models.py

--- Riki/wiki/web/models.py
+++ Riki/wiki/web/models.py
@@ -9,3 +9,9 @@ class ShoppingInfo(db.Model):
     zipcode = db.Column(db.String(10))
     email = db.Column(db.String(100))
     phone_number = db.Column(db.String(20))
+
+class HomeDatabase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    icon = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
